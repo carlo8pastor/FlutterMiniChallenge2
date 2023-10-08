f043d0fc4eab38d8ab471b97c2897586028006b2
Carlo <cpastor.ieu2020@student.ie.edu>
2023-10-08 22:22:33 +0200

    Complete app w/o mobile optimisation or flex

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       create: (context) => MyAppState(),
       child: MaterialApp(
         title: 'Flutter Demo',
-        theme: ThemeData(
+        theme: ThemeData.from(
           colorScheme: ColorScheme.fromSwatch(
             primarySwatch: Colors.blue,
             backgroundColor: Colors.white,
@@ -36,22 +37,28 @@ class Task {
 
 class MyAppState extends ChangeNotifier {
   int selectedIndex = 0;
-  List<Task> pendingTasks = [];
-  List<Task> completedTasks = [];
+  final List<Task> pendingTasks = [];
+  final List<Task> completedTasks = [];
 
   void selectPage(int index) {
     selectedIndex = index;
     notifyListeners();
   }
 
-  void addTask(String title) {
-    pendingTasks.add(Task(title, false));
+  void addTask(Task task) {
+    pendingTasks.add(task);
     notifyListeners();
   }
 
   void completeTask(int index) {
-    completedTasks.add(pendingTasks[index]);
-    pendingTasks.removeAt(index);
+    completedTasks.add(pendingTasks.removeAt(index));
+    notifyListeners();
+  }
+
+  void markTaskIncomplete(int index) {
+    Task task = completedTasks.removeAt(index);
+    task.completed = false;
+    pendingTasks.add(task);
     notifyListeners();
   }
 }
@@ -64,20 +71,24 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final TextEditingController dueDateController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     final appState = context.watch<MyAppState>();
 
     Widget page;
     String appBarText;
+    bool showAddButton = false;
 
     switch (appState.selectedIndex) {
       case 0:
-        page = const PendingTasks();
+        page = const TaskList(isPending: true);
         appBarText = 'Pending Tasks';
+        showAddButton = true;
         break;
       case 1:
-        page = const CompletedPage();
+        page = const TaskList(isPending: false);
         appBarText = 'Completed Tasks';
         break;
       default:
@@ -114,7 +125,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   flexibleSpace: FlexibleSpaceBar(
                     title: Text(
                       appBarText,
-                      style: TextStyle(
+                      style: const TextStyle(
                         color: Colors.white,
                       ),
                     ),
@@ -128,40 +139,82 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _showAddTaskDialog(context, appState);
-        },
-        child: const Icon(Icons.add),
-      ),
+      floatingActionButton: showAddButton
+          ? FloatingActionButton(
+              onPressed: () {
+                _showAddTaskDialog(context, appState);
+              },
+              child: const Icon(Icons.add),
+            )
+          : null,
     );
   }
 
   Future<void> _showAddTaskDialog(BuildContext context, MyAppState appState) async {
-    final TextEditingController taskController = TextEditingController();
+    final TextEditingController titleController = TextEditingController();
+    final TextEditingController descriptionController = TextEditingController();
+    DateTime? selectedDate;
 
     await showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
           title: Text('Add Task'),
-          content: TextField(
-            controller: taskController,
-            decoration: InputDecoration(hintText: 'Enter task'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              TextField(
+                controller: titleController,
+                decoration: const InputDecoration(labelText: 'Task Title'),
+              ),
+              TextField(
+                controller: descriptionController,
+                decoration: const InputDecoration(labelText: 'Task Description'),
+              ),
+              TextButton(
+                onPressed: () async {
+                  final pickedDate = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime.now(),
+                    lastDate: DateTime(2101),
+                  );
+
+                  if (pickedDate != null) {
+                    setState(() {
+                      selectedDate = pickedDate;
+                      dueDateController.text = DateFormat('yyyy-MM-dd').format(selectedDate!);
+                    });
+                  }
+                },
+                child: Text(selectedDate != null
+                    ? DateFormat('yyyy-MM-dd').format(selectedDate!)
+                    : 'Select Due Date'),
+              ),
+              TextField(
+                controller: dueDateController,
+                enabled: false,
+                decoration: const InputDecoration(labelText: 'Due Date'),
+              ),
+            ],
           ),
           actions: <Widget>[
             TextButton(
-              child: Text('Cancel'),
+              child: const Text('Cancel'),
               onPressed: () {
                 Navigator.of(context).pop();
               },
             ),
             TextButton(
-              child: Text('Add'),
+              child: const Text('Add'),
               onPressed: () {
-                String taskTitle = taskController.text.trim();
+                String taskTitle = titleController.text.trim();
+                String taskDescription = descriptionController.text.trim();
+                String taskDueDate = dueDateController.text.trim();
+
                 if (taskTitle.isNotEmpty) {
-                  appState.addTask(taskTitle);
+                  Task task = Task('$taskTitle\n$taskDescription\nDue: $taskDueDate', false);
+                  appState.addTask(task);
                 }
                 Navigator.of(context).pop();
               },
@@ -173,43 +226,64 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class PendingTasks extends StatelessWidget {
-  const PendingTasks({Key? key});
+class TaskList extends StatelessWidget {
+  const TaskList({Key? key, required this.isPending}) : super(key: key);
+
+  final bool isPending;
 
   @override
   Widget build(BuildContext context) {
     final appState = context.watch<MyAppState>();
+    final tasks = isPending ? appState.pendingTasks : appState.completedTasks;
 
     return ListView.builder(
-      itemCount: appState.pendingTasks.length,
+      itemCount: tasks.length,
       itemBuilder: (context, index) {
-        return ListTile(
-          title: Text(appState.pendingTasks[index].title),
-          leading: Checkbox(
-            value: appState.pendingTasks[index].completed,
-            onChanged: (value) {
-              appState.completeTask(index);
-            },
-          ),
+        final task = tasks[index];
+        final titleStyle = const TextStyle(
+          fontSize: 18.0,
+          fontWeight: FontWeight.bold,
         );
-      },
-    );
-  }
-}
+        final descriptionStyle = const TextStyle(
+          fontSize: 14.0,
+          color: Colors.grey,
+        );
+        final dueDateStyle = const TextStyle(
+          fontSize: 14.0,
+          color: Colors.grey,
+        );
 
-class CompletedPage extends StatelessWidget {
-  const CompletedPage({Key? key});
-
-  @override
-  Widget build(BuildContext context) {
-    final appState = context.watch<MyAppState>();
-
-    return ListView.builder(
-      itemCount: appState.completedTasks.length,
-      itemBuilder: (context, index) {
         return ListTile(
-          title: Text(appState.completedTasks[index].title),
-          leading: Icon(Icons.check),
+          title: Text(
+            task.title.split('\n')[0],
+            style: titleStyle,
+          ),
+          subtitle: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                task.title.split('\n')[1],
+                style: descriptionStyle,
+              ),
+              Text(
+                task.title.split('\n')[2],
+                style: dueDateStyle,
+              ),
+            ],
+          ),
+          leading: isPending
+              ? Checkbox(
+                  value: task.completed,
+                  onChanged: (value) {
+                    appState.completeTask(index);
+                  },
+                )
+              : IconButton(
+                  icon: const Icon(Icons.close),
+                  onPressed: () {
+                    appState.markTaskIncomplete(index);
+                  },
+                ),
         );
       },
     );
